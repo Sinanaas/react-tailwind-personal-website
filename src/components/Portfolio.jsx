87b3267d807d00769521452590e0cdbf34752b64
M Sinan Abdul Syakur <84369115+Sinanaas@users.noreply.github.com>
2023-09-30 01:49:25 +0700

    Fixed sizing issues

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,13 +25,13 @@ const Portfolio = () => {
   return (
     <div className="container mx-auto p-10 md:p-20 md:px-[8%] min-h-[100vh] flex flex-col items-center justify-center">
         <h1 className="text-4xl md:text-6xl mb-8 md:mb-16 text-amber-100 font-semibold underline underline-offset-8">PORTFOLIO</h1>
-        <div className={model ? 'fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80' : 'hidden'}>
-            <div className="md:flex max-w-[90%]  h-auto md:h-auto md:max-h-[80%] dark:bg-neutral-800 rounded-lg p-10 border-2 dark:border-neutral-900 mb-20">
+        <div className={model ? 'fixed inset-0 z-50 flex items-center justify-center bg-black/80' : 'hidden'}>
+            <div className="md:flex max-w-[90%]  h-auto max-h-[72%] md:h-auto md:max-h-[80%] dark:bg-neutral-800 rounded-lg py-4 px-8 border-2 dark:border-neutral-900 mb-32">
                 <div className="modal-image flex flex-col justify-center items-center md:w-[60%]">
                     <h2 className='font-bold text-amber-100 text-lg md:text-2xl mb-2 md:hidden'>{tempTitle}</h2>
                     <img src={tempimgSrc} alt={tempDesc} className="rounded h-full w-full drop-shadow-xl aspect-video object-cover md:object-contain h-full" />
                 </div>
-                <div className="flex flex-col modal-details md:ml-4 w-full text-amber-50 md:text-left pt-4 md:pt-0 md:w-[40%]">
+                <div className="flex flex-col modal-details md:ml-4 w-full text-amber-50 md:text-left  md:pt-0 md:w-[40%]">
                     <h2 className='font-bold underline underline-offset-8 text-2xl mb-2 text-amber-100 hidden md:block'>{tempTitle}</h2>
                     <h2 className="text-base text-left text-left hidden text-amber-50 md:block overflow-y-auto">{tempDesc}</h2>
                     <a href={tempLink} className="my-4" target='_blank'>
