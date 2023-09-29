920e905a61cad2a576113e3de342f185436c6384
M Sinan Abdul Syakur <84369115+Sinanaas@users.noreply.github.com>
2023-09-29 22:16:32 +0700

    Resizing the model for card pop-up

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -26,18 +26,18 @@ const Portfolio = () => {
     <div className="container mx-auto p-10 md:p-20 md:px-[8%] min-h-[100vh] flex flex-col items-center justify-center">
         <h1 className="text-4xl md:text-6xl mb-8 md:mb-16 text-amber-100 font-semibold underline underline-offset-8">PORTFOLIO</h1>
         <div className={model ? 'fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80' : 'hidden'}>
-            <div className="md:flex max-w-[80%] max-h-[100%] md:max-h-[80%] dark:bg-neutral-800 rounded-lg p-10 border-2 dark:border-neutral-900 transition">
+            <div className="md:flex max-w-[80%] max-h-[80%] md:max-h-[80%] dark:bg-neutral-800 rounded-lg p-10 border-2 dark:border-neutral-900 transition">
                 <div className="modal-image flex flex-col justify-center items-center md:w-[60%]">
-                    <h2 className='font-bold text-amber-100 text-lg md:text-2xl mb-2 md:hidden'>{tempTitle}</h2>
+                    <h2 className='font-bold text-amber-100 text-lg md:text-2xl  md:hidden'>{tempTitle}</h2>
                     <img src={tempimgSrc} alt={tempDesc} className="rounded h-full w-full drop-shadow-xl aspect-square md:aspect-square object-cover md:object-contain h-full" />
                 </div>
-                <div className="flex flex-col modal-details md:ml-4 w-full text-amber-50 md:text-left pt-4 md:pt-0 md:w-[40%]">
+                <div className="flex flex-col modal-details md:ml-4 w-full text-amber-50 md:text-left  md:pt-0 md:w-[40%]">
                     <h2 className='font-bold underline underline-offset-8 text-2xl mb-2 text-amber-100 hidden md:block'>{tempTitle}</h2>
                     <h2 className="text-base text-left text-left hidden text-amber-50 md:block overflow-y-auto">{tempDesc}</h2>
                     <a href={tempLink} className="my-4" target='_blank'>
                         <button className="uppercase bg-amber-500 md:w-40 w-full hover:bg-amber-600 text-white font-bold py-2 px-4 rounded shadow-lg">Visit Link</button>
                     </a>
-                    <h2 className="md:text-xl text-sm text-left md:text-center sm:hidden block overflow-y-auto max-h-56 mb-4">{tempDesc}</h2>
+                    <h2 className="md:text-xl text-sm text-left md:text-center sm:hidden block overflow-y-auto max-h-24 mb-4">{tempDesc}</h2>
                 </div>
                 <div className="close-button justify-end">
                     <FontAwesomeIcon icon={faClose} className="text-gray-500 cursor-pointer" size="2x" onClick={closeModal} />
