5cad8ad77c35bccde8e1622c09150c843a5eaf65
M Sinan Abdul Syakur <84369115+Sinanaas@users.noreply.github.com>
2024-05-17 15:36:13 +0700

    add loader for sinan image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import sinanImg from '../../public/assets/foto2.png';
 import './Home.css';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState('#ffffff');
+
+  const handleImageLoaded = () => {
+    setLoading(false);
+  }
+
+  const handleImageError = () => {
+    setLoading(false);
+  }
+
   return (
     <div className='md:p-0 home-sections bg-opacity-0 container mx-auto md:h-[100vh] md:flex items-center justify-center'>
       <div className="pt-28 p-8 md:p-0 md:pt-16 md:pb-0 pb-8 w-full md:flex text-amber-50 justify-center items-center">
@@ -10,8 +22,19 @@ const Home = () => {
           <h1 className='font-bold md:text-6xl lg:text-7xl text-4xl mb-4'>Hello There!</h1>
           <p className='text-xl md:text-lg font-semibold mx-auto'>My name is Muhammad Sinan Abdussyakur, an undergraduate student at Bina Nusantara University majoring in Computer Science and currently living in Jakarta, Indonesia. Welcome to my portfolio :)</p>
         </div>
-        <div className="lg:w-[30vw] md:w-[40vw] flex items-center justify-center mt-6"> 
-          <img src={sinanImg} alt="Muhammad Sinan Abdussyakur" className="md:w-full w-96 h-auto " /> 
+        <div className="lg:w-[30vw] md:w-[40vw] flex items-center justify-center mt-6">
+          {loading && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <ClipLoader color={color} loading={loading} size={50} />
+            </div>
+          )}
+          <img
+            src={sinanImg}
+            alt="Muhammad Sinan Abdussyakur"
+            className={`md:w-full w-96 h-auto ${loading ? 'hidden' : 'block'}`}
+            onLoad={handleImageLoaded}
+            onError={handleImageError}
+          />
         </div>
       </div>
     </div>
